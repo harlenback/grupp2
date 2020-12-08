ff05410de3234e1329a8b444741eb137fa869e7a
harlenback <harlenback@gmail.com>
2020-12-08 08:50:53 +0100

    made a calcSum function and style css for border-collaps

---
+++ Yatzy/yatzy.js
@@ -0,0 +1,15 @@
+function calcSum() {
+    let playerone1 = document.getElementById("playerone_ones");
+    let playerone2 = document.getElementById("playerone_twos");
+    let playerone3 = document.getElementById("playerone_threes");
+    let playerone4 = document.getElementById("playerone_fours");
+    let playerone5 = document.getElementById("playerone_fives");
+    let playerone6 = document.getElementById("playerone_sixes");
+  
+    let sum = Number(playerone1.value) + Number(playerone2.value) + Number(playerone3.value) + Number(playerone4.value) + Number(playerone5.value) + Number(playerone6.value);
+
+    document.getElementById("playerone_sum").innerHTML = sum;
+    if (sum > 63) {
+      document.getElementById("playerone_bonus").innerHTML = 50;
+    }
+}
