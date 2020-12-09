2485a518b0177f658fdc4c8110023f29a6f5831d
simonswanholt01 <simonswanholt@gmail.com>
2020-12-09 11:49:20 +0100

    Added Lab2 to Group folder

---
+++ lab2/nim.js
@@ -0,0 +1,42 @@
+
+class Player{
+    constructor(name,points,turn){
+        this.name;
+        this.points = ["I","I","I","I","I","I","I","I","I","I","I","I","I","I","I","I","I","I","I","I","I",];
+        this.turn;
+    }
+    PlayerTakeAway1(){
+        this.points.splice(0,1)
+        Scoreupdate()
+        
+    }
+    PlayerTakeAway2(){
+        this.points.splice(0,2)
+        Scoreupdate()
+    }
+   PlayerTakeAway3(){
+        this.points.splice(0,3)
+         Scoreupdate()
+    }
+
+}
+
+
+
+let Player1 = new Player;
+let Player2 = new Player;
+
+
+
+
+
+function Scoreupdate(){
+    let P1_points = document.getElementById("player1_points");
+    P1_points.innerHTML = Player1.points.join(" ");
+    let P2_points = document.getElementById("player2_points");
+    P2_points.innerHTML = Player2.points.join(" ");
+}
+Scoreupdate()
+
+
+
