b4e0814550d7ee9184a6210cff9ed1ecd2e40ea8
harlenback <harlenback@gmail.com>
2020-12-10 08:12:58 +0100

    added martintest

---
+++ MartinNIMtest/script.js
@@ -0,0 +1,49 @@
+class Game {
+    constructor(points = 21) {
+        this.points = points;
+    }
+
+        PlayerTake1() {
+            this.points -=1;
+            Scoreupdate()        
+        }
+    
+        PlayerTake2(){
+            this.points -=2;
+            Scoreupdate()
+        }
+        
+        PlayerTake3(){
+            this.points -=3;
+             Scoreupdate()
+        }
+}
+
+let Game1 = new Game();
+
+class Player{
+    constructor(name, playerID, turn){
+        this.name = name;
+        this.playerID = playerID;
+        this.turn = turn;
+    }
+}
+
+let Player1 = new Player(document.getElementById('player1Name'))
+let Player2 = new Player(document.getElementById('player2Name'))
+
+
+function setPlayerName(){        
+    let player1Name = document.getElementById('player1Name');
+    let player2Name = document.getElementById('player2Name');
+  
+    document.getElementById('player1NameLabel').innerHTML = player1Name.value;
+    document.getElementById('player2NameLabel').innerHTML = player2Name.value;
+
+    Player1.name = player1Name.value;
+    Player2.name = player2Name.value;
+}
+
+function Scoreupdate(){
+     document.getElementById("gamePoints").innerHTML = Game1.points;
+}
